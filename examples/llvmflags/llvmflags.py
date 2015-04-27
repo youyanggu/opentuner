1f740c2612abcb0b23484f9c7bf54b2bcaa7b97a
Youyang Gu <yygu@lanka.csail.mit.edu>
2015-04-27 05:21:20 -0400

    fixed bugs in llvmflags

--- examples/llvmflags/llvmflags.py
+++ examples/llvmflags/llvmflags.py
@@ -6,6 +6,11 @@
 #
 import adddeps  # fix sys.path
 
+import argparse
+import logging
+import os
+import shutil
+
 import opentuner
 from opentuner import ConfigurationManipulator
 from opentuner import EnumParameter
@@ -17,21 +22,21 @@ from opentuner import Result
 CLANGXX_PATH = '/data/scratch/yygu/build/Debug+Asserts/bin/clang++'
 
 DEFAULT_PARAM_VALUE = 1
-USE_ONLY_INTERNAL = True
-USE_ONLY_EXTERNAL = True
+MIN_VAL = 0
+MAX_VAL = 1000
 PARAMS_INTERNAL_FILE = 'params_internal.txt'
 PARAMS_EXTERNAL_FILE = 'params_external.txt'
 FLAGS_INTERNAL_FILE = 'flags_internal.txt'
 FLAGS_EXTERNAL_FILE = 'flags_external.txt'
 
-OUTPUT_FILE = './tmp.bin'
 PREPEND_FLAG = "-mllvm "
-APP = 'apps/raytracer.cpp'
+
+log = logging.getLogger('llvmflags')
 
 argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
 argparser.add_argument('source', help='source file to compile')
 argparser.add_argument('--compile-template',
-                       default='{clang} {source} -o {output} {flag}',
+                       default='{clang} {source} -o {output} {flags}',
                        help='command to compile {source} into {output} with'
                             ' {flags}')
 argparser.add_argument('--output', default='./tmp.bin',
@@ -43,6 +48,10 @@ argparser.add_argument('--force-killall', action='store_true',
                        help='killall cc1plus before each collection')
 argparser.add_argument('--memory-limit', default=1024 ** 3, type=int,
                        help='memory limit for child process')
+argparser.add_argument('--logfile', default=None,
+		       help='log file')
+argparser.add_argument('--use-internal', action='store_true') 
+argparser.add_argument('--use-external', action='store_true') 
 
 class LlvmFlagsTuner(MeasurementInterface):
 
@@ -56,6 +65,8 @@ class LlvmFlagsTuner(MeasurementInterface):
     self.llvm_flags = self.extract_working_flags()
     self.llvm_params = self.extract_working_params()
 
+    # this flag is necessary so that each new command is compiled each time
+    self.parallel_compile = True
     try:
       os.stat('./tmp')
     except OSError:
@@ -64,7 +75,7 @@ class LlvmFlagsTuner(MeasurementInterface):
 
   def run_baselines(self):
     log.info("baseline perfs -O0=%.4f -O1=%.4f -O2=%.4f -O3=%.4f",
-             *[self.run_with_flags(['-O%d' % i], None).time
+             *[self.run_with_flags(['-O{}'.format(i)], None).time
                for i in range(4)])
 
   def convert_flags(self, fname):
@@ -78,19 +89,19 @@ class LlvmFlagsTuner(MeasurementInterface):
     params = []
     with open(fname) as f:
       for line in f:
-        params.append((line[:-1], 0, 1000))
+        params.append(line[:-1])
     return params
 
   def check_if_flag_works(self, flag):
     if flag not in ['O0', 'O1', 'O2', 'O3']:
       flag = 'mllvm -' + flag
+    flag = '-' + flag
     cmd = args.compile_template.format(source=args.source, output=args.output,
-                                       flag='-'+flag, clang=args.clang)
+                                       flags=flag, clang=args.clang)
     compile_result = self.call_program(cmd, limit=args.compile_limit)
     if compile_result['returncode'] != 0:
-      log.warning("removing flag %s because it results in compile error", flag)
+      log.warning("removing flag {} because it results in compile error".format(flag))
       return False
-    print compile_result
     return True
 
   def extract_working_flags(self):
@@ -99,13 +110,12 @@ class LlvmFlagsTuner(MeasurementInterface):
     each one.
     """
     all_flags = []
-    if USE_ONLY_INTERNAL:
-      all_flags = self.llvm_flags_internal
-    elif USE_ONLY_EXTERNAL:
-      all_flags = self.llvm_flags_external
-    else:
-      all_flags = self.llvm_flags_internal + self.llvm_flags_external
-    working_flags = filter(check_if_flag_works, all_flags)
+    if args.use_internal:
+      all_flags += self.llvm_flags_internal
+    if args.use_external:
+      all_flags += self.llvm_flags_external
+
+    working_flags = filter(self.check_if_flag_works, all_flags)
     return working_flags
 
   def extract_working_params(self):
@@ -114,16 +124,14 @@ class LlvmFlagsTuner(MeasurementInterface):
     each one.
     """
     all_params = []
-    if USE_ONLY_INTERNAL:
-      all_params = self.llvm_params_internal
-    elif USE_ONLY_EXTERNAL:
-      all_params = self.llvm_params_external
-    else:
-      all_params = self.llvm_params_internal + self.llvm_params_external
+    if args.use_internal:
+      all_params += self.llvm_params_internal
+    elif args.use_external:
+      all_params += self.llvm_params_external
 
     working_params = []
     for param in all_params:
-      if self.check_if_flag_works('-{}={}'.format(param, DEFAULT_PARAM_VALUE)):
+      if self.check_if_flag_works('{}={}'.format(param, DEFAULT_PARAM_VALUE)):
           working_params.append(param)
     return working_params
 
@@ -139,13 +147,13 @@ class LlvmFlagsTuner(MeasurementInterface):
       manipulator.add_parameter(
         EnumParameter(flag,
                       ['on', 'off', 'default']))
-    for param, min, max in self.llvm_params:
+    for param in self.llvm_params:
       manipulator.add_parameter(
-        IntegerParameter(param, min, max))
+        IntegerParameter(param, MIN_VAL, MAX_VAL))
     return manipulator
 
   def cfg_to_flags(self, cfg):
-    flags = ['-O%d' % cfg['-O']]
+    flags = ['-O%d' % cfg['opt_level']]
     for flag in self.llvm_flags:
       if cfg[flag] == 'on':
         flags.append(PREPEND_FLAG + '-' + flag)
@@ -156,7 +164,7 @@ class LlvmFlagsTuner(MeasurementInterface):
     return flags
 
   def get_tmpdir(self, result_id):
-    return './tmp/%d' % result_id
+    return './tmp/{}'.format(result_id)
 
   def cleanup(self, result_id):
     tmp_dir = self.get_tmpdir(result_id)
@@ -164,33 +172,6 @@ class LlvmFlagsTuner(MeasurementInterface):
 
   def run(self, desired_result, input, limit):
     pass
-    """
-    cfg = desired_result.configuration.data
-    llvm_cmd = CLANGXX_PATH + ' ' + APP + ' -o ' + OUTPUT_FILE
-    llvm_cmd += ' -O{0} '.format(cfg['opt_level'])
-
-    for flag in self.llvm_flags:
-      if cfg[flag] == 'on':
-        llvm_cmd += PREPEND_FLAG + '-{0} '.format(flag)
-      elif cfg[flag] == 'off':
-        continue
-    for param, min, max in self.llvm_params:
-      llvm_cmd += PREPEND_FLAG + '-{}={} '.format(param, cfg[param])
-    
-    print llvm_cmd
-
-    compile_result = self.call_program(llvm_cmd)
-    if compile_result['returncode'] != 0:
-      #temp solution
-      return Result(state='ERROR', time=float('inf'))
-
-    run_result = self.call_program(OUTPUT_FILE)
-    print run_result
-    if run_result['returncode'] != 0:
-      return Result(state='ERROR', time=float('inf'))
-      
-    return Result(time=run_result['time'])
-    """
 
   compile_results = {'ok': 0, 'timeout': 1, 'error': 2}
 
@@ -226,7 +207,6 @@ class LlvmFlagsTuner(MeasurementInterface):
     return Result(time=run_result['time'])
 
   def compile_with_flags(self, flags, result_id):
-    print flags
     tmp_dir = self.get_tmpdir(result_id)
     try:
       os.stat(tmp_dir)
@@ -248,8 +228,16 @@ class LlvmFlagsTuner(MeasurementInterface):
         self.debug_gcc_error(flags)
         return self.compile_results['error']
     return self.compile_results['ok']
+ 
+  def run_with_flags(self, flags, limit):
+    return self.run_precompiled(None, None, limit, self.compile_with_flags(flags, 0), 0)
 
 if __name__ == '__main__':
-  argparser = opentuner.default_argparser()
+  opentuner.init_logging()
   args = argparser.parse_args()
+  if args.logfile:
+    fh = logging.FileHandler(args.logfile)
+    fh.setLevel(logging.INFO)
+    log.addHandler(fh)
+    logging.getLogger('opentuner.search.plugin.DisplayPlugin').addHandler(fh)
   LlvmFlagsTuner.main(args)
